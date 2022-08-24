0396f13555d1e18dc341afa6f05303cf6c9d485f
jacob-d242 <jacobmoracha@gmail.com>
2022-08-24 16:41:54 +0300

    updating bottom tabs

--- src/constants/Index.js
+++ src/constants/Index.js
@@ -9,6 +9,7 @@ export const COLORS={
 
 export const IMAGES = {
     user: require("../assets/images/user.png"),
-    gas: require("../assets/images/gas.png"),
-    Fav: require("../assets/images/History.png")
+    gas: require("../assets/images/Gas.png"),
+    Fav: require("../assets/images/History.png"),
+    gallon: require("../assets/images/gallon.png")
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,13 +2,15 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from '../components/Home'
+import BottomNav from '../components/BottomNav'
+import BottomTabs from '../navigation/BottomTabs'
 
 const HomeScreen = () => {
     const Stack = createNativeStackNavigator()
   return (
     <>
       <Stack.Navigator>
-          <Stack.Screen name="HomeComponent" component={Home} options={{ headerShown: false }} />
+          <Stack.Screen name="HomeComponent" component={BottomTabs} options={{ headerShown: false }} />
       </Stack.Navigator>
     </>
   )
