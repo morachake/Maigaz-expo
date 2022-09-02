17e8b2ce83cb261a70d76269b59164b70b36102d
DevHumbleChris <dev.humblechris254@gmail.com>
2022-09-02 13:28:14 +0300

    Updated cartSlice

--- src/store/slices/cartSlice.js
+++ src/store/slices/cartSlice.js
@@ -9,10 +9,13 @@ export const cartSlice = createSlice({
         addProductToCart: (state, action) => {
             const availableProduct = state.cartItems.filter(cartItem => cartItem.id === action.payload.id)
             if (availableProduct.length <= 0) {
-                const newCart = [...state.cartItems, action.payload]
+                const product = { ...action.payload, quantity: 1 }
+                const newCart = [...state.cartItems, product]
                 state.cartItems = newCart
+                console.log(state.cartItems)
             } else {
                 state.cartItems = state.cartItems
+                console.log(state.cartItems)
             }
         }
     }
