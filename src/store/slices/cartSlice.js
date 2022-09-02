7fd352e66614cf2ab2688a725cfbcf75c4c3934f
DevHumbleChris <dev.humblechris254@gmail.com>
2022-09-02 15:35:31 +0300

    Fixed all Merge and Pull Errors

--- src/store/slices/cartSlice.js
+++ src/store/slices/cartSlice.js
@@ -3,14 +3,23 @@ import { createSlice } from "@reduxjs/toolkit";
 export const cartSlice = createSlice({
     name: 'cartSlice',
     initialState: {
-        cart: []
+        cartItems: []
     },
     reducers: {
-        addToCart: (state, action) => {
-            console.log(action)
+        addProductToCart: (state, action) => {
+            const availableProduct = state.cartItems.filter(cartItem => cartItem.id === action.payload.id)
+            if (availableProduct.length <= 0) {
+                const product = { ...action.payload, quantity: 1 }
+                const newCart = [...state.cartItems, product]
+                state.cartItems = newCart
+                console.log(state.cartItems)
+            } else {
+                state.cartItems = state.cartItems
+                console.log(state.cartItems)
+            }
         }
     }
 })
 
-export const { addToCart } = cartSlice.actions
+export const { addProductToCart } = cartSlice.actions
 export default cartSlice.reducer
